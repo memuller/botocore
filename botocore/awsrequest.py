7d33f29ad19d7a1fc11fdc3e955ce813be8ce46a
Emed <Mohamed.Berrada@Outlook.com>
2021-05-08 04:18:36 +0200

    fix: custom domain case

--- botocore/awsrequest.py
+++ botocore/awsrequest.py
@@ -33,6 +33,9 @@ from botocore.exceptions import UnseekableStreamError
 logger = logging.getLogger(__name__)
 
 
+from urllib.parse import urlparse 
+
+
 class AWSHTTPResponse(HTTPResponse):
     # The *args, **kwargs is used because the args are slightly
     # different in py2.6 than in py2.7/py3.
@@ -250,6 +253,12 @@ def prepare_request_dict(request_dict, endpoint_url, context=None,
         headers = r['headers']
         headers['User-Agent'] = user_agent
     host_prefix = r.get('host_prefix')
+
+    # TODO: BETTER HANDLE THE CUSTOM DOMAIN CASE     
+    endpoint_url_domain = urlparse(endpoint_url).netloc        
+    if endpoint_url_domain in r['url_path']:         
+        r['url_path'] = r['url_path'].replace('/{}'.format(endpoint_url_domain), '')
+     
     url = _urljoin(endpoint_url, r['url_path'], host_prefix)
     if r['query_string']:
         # NOTE: This is to avoid circular import with utils. This is being
